176be191db1c6f739d77af1aa0fc0e2170926a87
anastasiaserg183 <52037836+anastasiaserg183@users.noreply.github.com>
2020-12-03 23:45:30 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ let mainBackground = document.getElementById('main-change-background');
 let mainSecondBackground = document.getElementById('main-background_second-layer');
 let arrowLeft = document.getElementById('left-arrow');
 
-/*let arrows = document.getElementsByClassName('arrow');*/
 let arrowsContainer = document.getElementById('arrows');
 
 let i = 0;
@@ -13,14 +12,14 @@ let timer;
 let timePassed;
 let height = document.documentElement.scrollHeight;
 console.log('height ' + height);
-
+let intervalHeight = 0;
 
 
 changeBackground(); //вызываем функцию для первого изображения, потому что таймер еще не срабатывает в этот момент
 
 function changeFirstLayer() {
     mainBackground.style.background = 'url(images/main-container/' + ++i + '.jpg)';
-    
+
     if (i == 4) {
         i = 0;
     }
@@ -34,74 +33,64 @@ function changeSecondLayer() {
     }
 }
 
+
+
 function smoothAnimationVertical() {
     mainBackground.style.bottom = 0 + 'px';
-    let intervalWidth = height/25;
+    intervalHeight = height / 25;
     j = 1;
-    
-    start = Date.now(); // запомнить время начала
-    
-    
-    function animationOfChanging() {
-       // сколько времени прошло с начала анимации?
-        timePassed = Date.now() - start;
-        
 
-        if (timePassed >= 4975) {
-            clearInterval(timer); // закончить анимацию через 3 секунды
-            return;
-        } else if (timePassed >= 4500) {
-            draw(timePassed); 
-        } else {}  
-    }
+    start = Date.now(); // запомнить время начала
+
+    animationOfChanging();
+
 
     // в то время как timePassed идёт от 0 до 5000
 
-    function draw(timePassed) {
-        mainBackground.style.bottom = intervalWidth * j++ + 'px';
-    }
-    
     timer = window.setInterval(animationOfChanging, 20);
 }
 
+function animationOfChanging() {
+    // сколько времени прошло с начала анимации?
+    timePassed = Date.now() - start;
 
+
+    if (timePassed >= 4975) {
+        clearInterval(timer); // закончить анимацию через 3 секунды
+        return;
+    } else if (timePassed >= 4500) {
+        draw(intervalHeight);
+    } else {}
+}
+
+function draw(intervalHeight) {
+    mainBackground.style.bottom = intervalHeight * j++ + 'px';
+}
 
 function changeBackground() {
-    
+
     changeFirstLayer();
     changeSecondLayer();
-    smoothAnimationVertical();   
+    smoothAnimationVertical();
 }
 
 let mainTimer = window.setInterval(changeBackground, 5000);
 
 
+
 function arrowChangeBackgroundLeft(event) {
+    clearAnimationBackground();
+    this.style.background = 'white';
+    
+}
+
+function clearAnimationBackground() {
     clearInterval(mainTimer);
     clearInterval(timer);
-    this.style.background = 'white';
 }
 
 
 arrowLeft.addEventListener('click', arrowChangeBackgroundLeft);
-arrowRight.addEventListener('click', arrowChangeBackgroundRight);
+/*arrowRight.addEventListener('click', arrowChangeBackgroundRight);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
